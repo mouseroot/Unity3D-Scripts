d607115b4cfbc0d99565d344287834c78e698f5b
Souradeep-Nanda <souradeep_nanda@hotmail.com>
2014-05-30 14:05:30 +0530

    Added ObjectPooling scripts
    For more information, see the official unity tutorial
    https://www.youtube.com/watch?v=9-zDOJllPZ8

---
+++ ObjectPooling/MoveForward.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour {
+	// Moves the bullet forward
+	//
+	// This script can be replaced without breaking the 
+	// object pooling mechanism
+
+	public float speed = 20f;
+	void Update(){
+		transform.position = new Vector3(0f,0f,transform.position.z+speed*Time.deltaTime);
+	}
+}
---
+++ ObjectPooling/ObjectPooling.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+Object Pooling tutorial here
+https://www.youtube.com/watch?v=9-zDOJllPZ8
+
+IMPORTANT: The bullet prebab must have the bullet.cs script to work properly
+ */
+
+public class ObjectPooling : MonoBehaviour {
+
+	public GameObject bullet;// The object to be pooled
+	public int listSize = 0; // size of the pool
+	public bool isListExpandable = true; // if more objects are required
+
+	private List<GameObject> objectList; // the list to store the objects
+
+	void Awake(){
+		objectList = new List<GameObject>();
+		// Initialise all the objects in the pool
+		for(int i = 0; i < listSize; i++){
+			GameObject obj = (GameObject)Instantiate(bullet);
+			obj.SetActive(false);
+			objectList.Add(obj);
+		}
+	}
+
+	// Change the update function to suit your game
+	void Update(){
+		if(Input.GetButtonDown("Jump")){
+			Invoke("Fire",1f);
+		}
+	}
+
+	// Instantiates the objects, it does not move the bullet
+	public void Fire(){
+		bool isObjectAvailable = false;
+		// Checks for an inactive gameobject and activates it
+		foreach(GameObject temp in objectList){ 
+			if(!temp.activeInHierarchy){
+				isObjectAvailable = true;
+				temp.transform.position = transform.position;
+				temp.transform.rotation = transform.rotation;
+				temp.SetActive(true);
+				break;
+			}			
+		}
+		// if there is no active game object AND the list is expandable,
+		// then expands the list
+		if(!isObjectAvailable && isListExpandable){ 
+			GameObject obj = (GameObject)Instantiate(bullet);
+			obj.transform.position = transform.position;
+			obj.transform.rotation = transform.rotation;
+			objectList.Add(obj);
+			obj.SetActive(true);
+		}
+	}
+
+}
